0c957958a7948e769be20d116160dbb8b3f948a3
mattpurcell <matthewpurcell@Matthews-MacBook-Air-3.local>
2016-10-16 19:36:10 -0400

    area volume

---
+++ area_volume.py
@@ -0,0 +1,8 @@
+radius = input("What's the radius: ")
+
+pi = 3.14
+
+volume = 4*pi*(radius**3)/3
+area = 4*pi*(radius**2)
+
+print "Given your radius, your volume is %d and your area is %d" % (volume, area) 
